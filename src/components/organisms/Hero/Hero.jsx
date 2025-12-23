f84490ba0ff00e3056d61f119426180c1d75ef8a
pranavwaykar <waykarpranav777@gmail.com>
2025-12-23 18:36:58 +0530

    Minor resume change

--- src/components/organisms/Hero/Hero.jsx
+++ src/components/organisms/Hero/Hero.jsx
@@ -355,7 +355,7 @@ const Hero = () => {
               size="large"
               onClick={() =>
                 window.open(
-                  "https://drive.google.com/file/d/12acNXf_NdZFDaGmrAfkQ1sABvvGVH7EX/view?usp=sharing",
+                  "https://drive.google.com/file/d/1WaEi4yVA-VkVL8ZBonTQ4Xkj8Ikm2ic6/view?usp=sharing",
                   "_blank",
                   "noopener,noreferrer"
                 )
